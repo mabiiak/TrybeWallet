a94cda58715405e7934a310ecee83bc4159f3387
mabiak <mabiane.polniak@gmail.com>
2022-02-23 22:51:39 -0300

    Updates

---
+++ precache-manifest.23b5b670f37150cff9d2d1891f6b79cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c4242c674ee8d781b23015b884d34a",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "57a330530fc5de569cb5",
+    "url": "/trybewallet/static/css/main.93a62104.chunk.css"
+  },
+  {
+    "revision": "492cff431c59cd2bd769",
+    "url": "/trybewallet/static/js/2.ab5d5c8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.ab5d5c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a330530fc5de569cb5",
+    "url": "/trybewallet/static/js/main.969e164a.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
