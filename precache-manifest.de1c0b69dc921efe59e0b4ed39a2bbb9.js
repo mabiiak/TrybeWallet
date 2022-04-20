42289ab2bb7d46aa7b57b0fe881b6f2e55c326b0
mabiak <mabiane.polniak@gmail.com>
2022-04-20 14:16:11 -0300

    Updates

---
+++ precache-manifest.de1c0b69dc921efe59e0b4ed39a2bbb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60652efd7cce75c2a66177f23d63bd89",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e7a900ce5e69e5469156",
+    "url": "./static/css/main.9bbacad3.chunk.css"
+  },
+  {
+    "revision": "457475618285aab2ab48",
+    "url": "./static/js/2.3bf00930.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3bf00930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a900ce5e69e5469156",
+    "url": "./static/js/main.ccbca35c.chunk.js"
+  },
+  {
+    "revision": "6ef1c7e4f51c36eaf084",
+    "url": "./static/js/runtime-main.b4f966c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trybewallet/precache-manifest.3a17a9f666af0160588e378d1a6a6aa3.js"
+  "./precache-manifest.de1c0b69dc921efe59e0b4ed39a2bbb9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trybewallet/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
